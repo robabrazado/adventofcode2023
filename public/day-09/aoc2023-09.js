7c1e02b584c2fb1aebeb9b2daa1623deb6bdb51b
Rob Abrazado <69860331+robabrazado@users.noreply.github.com>
2023-12-09 08:42:48 -0500

    Finished day 9 part 2

--- public/day-09/aoc2023-09.js
+++ public/day-09/aoc2023-09.js
@@ -1,13 +1,16 @@
 function part1(puzzleInput) {
     const lines = puzzleInput.split("\n");
 
-    return lines.map(line => pyramidPrediction(line.split(" ").map(e => parseInt(e))))
+    return lines.map(line => pyramidPredictionRight(line.split(" ").map(e => parseInt(e))))
         .reduce((a, f) => a + f, 0);
 }
 
 
 function part2(puzzleInput) {
+    const lines = puzzleInput.split("\n");
 
+    return lines.map(line => pyramidPredictionLeft(line.split(" ").map(e => parseInt(e))))
+        .reduce((a, f) => a + f, 0);
 }
 
 /**
@@ -16,7 +19,48 @@ function part2(puzzleInput) {
  * @param {number[]} history 
  * @return {number}
  */
-function pyramidPrediction(history) {
+function pyramidPredictionRight(history) {
+    const rows = buildPyramid(history);
+
+    // Add a trailing 0 to the bottom row
+    rows.at(-1).push(0);
+
+    // Starting at second-to-bottom row, add new elements going up
+    for (let i = rows.length - 2; i >= 0; i--) {
+        const newValue = rows[i].at(-1) + rows[i + 1].at(-1);
+        rows[i].push(newValue);
+    }
+
+    return rows[0].at(-1);
+}
+
+/**
+ * 
+ * 
+ * @param {number[]} history 
+ * @return {number}
+ */
+function pyramidPredictionLeft(history) {
+    const rows = buildPyramid(history);
+
+    // Add a leading 0 to the bottom row
+    rows.at(-1).unshift(0);
+
+    // Starting at second-to-bottom row, add new elements going up
+    for (let i = rows.length - 2; i >= 0; i--) {
+        const newValue = rows[i][0] - rows[i + 1][0];
+        rows[i].unshift(newValue);
+    }
+
+    return rows[0][0];
+}
+
+/**
+ * 
+ * @param {number[]} history 
+ * @return {number[][]}
+ */
+function buildPyramid(history) {
     const rows = [history];
 
     // Build the pyramid
@@ -30,15 +74,6 @@ function pyramidPrediction(history) {
         rows.push(newRow);
         prevRow = newRow;
     }
-    
-    // Add a trailing 0 to the bottom row
-    rows.at(-1).push(0);
 
-    // Starting at second-to-bottom row, add new elements going up
-    for (let i = rows.length - 2; i >= 0; i--) {
-        const newValue = rows[i].at(-1) + rows[i + 1].at(-1);
-        rows[i].push(newValue);
-    }
-
-    return rows[0].at(-1);
+    return rows;
 }
